03b0e0d3bc963070bbcbbafed72f2b5d01da7184
dimecoin <dimecoin@gmail.com>
2017-02-12 11:21:58 -0600

    The great refactoring, part 2

--- simulator.js
+++ simulator.js
@@ -6,17 +6,47 @@
 sim = new Simulator();
 
 function Simulator() {
-		var running = false;
+	this.running = false;
+
+	// CPUs are executed in turns, one after another
+	// Randomly pick one to go first, out of fairiness.
+	// true=cpu0s turn, false=cpu1s turn.
+	//
+	this.turn =  Math.random() >= 0.5;
+
+	// This is speed cpu runs at.  It's time between step
+	// So high value will run it slower.
+	this.cpuSpeed = 250;
+
 	
+}
+
+/**
+* called on page load.  Just make things easy 
+*/
+Simulator.prototype.init = function() {
+
+	//console.log("memory: " +memory);
+	//console.log("cpu0: " +JSON.stringify(cpu0));
+	//console.log("cpu1: " +JSON.stringify(cpu1));
+
+	loadProgram(0);
+	loadProgram(1);
+	this.display();
+
 }
 
 /**
 * Resets "everything" in simulation to clean slate.
 */ 
 Simulator.prototype.reset = function() {
+
+	turn =  Math.random() >= 0.5;
+
 	memory.reset();
 	cpu0.reset();
 	cpu1.reset();
+
 }
 
 /**
@@ -28,17 +58,47 @@ Simulator.prototype.display = function() {
 	cpu1.display();
 }
 
-
+/**
+* Halts the simulation.
+*/
 Simulator.prototype.halt = function() {
 	this.running=false;
-
-	// TODO: this interface junk should go some place else.
-	$("#button_runsim").prop("disabled",false);
-	$("#button_step").prop("disabled",false);
-	
 }
 
+/**
+* Runs one step in simulation.
+*/ 
+Simulator.prototype.runNextStep = function() {
 
+	if (cpu0.halted || cpu1.halted) {
+		return;
+	}
+
+	if (this.turn) {
+		cpu0.executeNext();
+	} else {
+		cpu1.executeNext();
+	}
+
+	this.turn=!this.turn;
+	this.display();
+}
+
+/**
+* Runs a continious simulation.
+*/
+
+Simulator.prototype.runLoop = function() {
+
+	if (!this.running) {
+		return;
+	}
+
+	this.runNextStep();
+
+	// This loops back and reruns sim and delays by cpuSpeed (step).
+	setTimeout( function () { sim.runLoop(); }, this.cpuSpeed);
+}
 
 
 
